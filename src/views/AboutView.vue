6747f2f4f6c86fb8e4fa2910d10e48290013a5c5
DoubleHong <greathongtian@gmail.com>
2023-03-06 15:35:34 +0800

    Hong

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,19 @@
 <template>
-
+  <div id="login">
+    <h2>输入学号查看信息:</h2>
+    <br>
+    <el-input v-model="stuNum" clearable maxlength="11" style="width: 80%" />
+    <br><br><br>
+    <el-button style="width: 80%;">查看</el-button>
+  </div>
 </template>
 
 <script lang="ts" setup>
+
+import {ref} from "vue";
+
+//学号
+const stuNum = ref('')
 
 </script>
 
