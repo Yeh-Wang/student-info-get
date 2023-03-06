fb86364f4f5287073bb92f6ddaf0f93d40395c52
DoubleHong <greathongtian@gmail.com>
2023-03-06 10:33:39 +0800

    Hong

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,15 @@
 <template>
-  <van-nav-bar title="学生信息录入"/>
-  <van-form @submit="onSubmit" v-model="form">
-    <van-field v-model="form.stuNumber" name="学号" label="学号" :rules="[{ required: true, message: '请填写学号' }]"/>
-    <van-field v-model="form.stuName" name="名字" label="名字" :rules="[{ required: true, message: '请填写姓名' }]"/>
-    <van-field
-        v-model="result"
-        is-link
-        readonly
-        name="datePicker"
-        label="生日"
-        placeholder="点击选择日期"
-        @click="showPicker = true"
-    />
-    <van-popup :show="showPicker" position="bottom">
-      <van-date-picker :min-date="minDate" :max-date="maxDate" @confirm="onConfirm" @cancel="showPicker = false" />
-    </van-popup>
-    <van-field name="radio" label="性别">
-      <template #input>
-        <van-radio-group v-model="form.sex" direction="horizontal">
-          <van-radio name="男">男</van-radio>
-          <van-radio name="女">女</van-radio>
-        </van-radio-group>
-      </template>
-    </van-field>
-    <van-field
-        v-model="form.telephone"
-        rows="2"
-        label="电话号码"
-        maxlength="11"
-        placeholder="请输入电话号码"
-        show-word-limit
-    />
-    <van-field
-        v-model="result_1"
-        is-link
-        readonly
-        name="area"
-        label="家庭住址"
-        placeholder="点击选择省市区"
-        @click="showArea = true"
-    />
-    <van-popup :show="showArea" position="bottom">
-      <van-area
-          :area-list="areaList"
-          @confirm="onConfirm_1"
-          @cancel="showArea = false"
-      />
-    </van-popup>
-    <van-field
-        v-model="form.qq"
-        rows="2"
-        label="QQ"
-        maxlength="11"
-        placeholder="请输入QQ号码"
-        show-word-limit
-    />
-    <div>
-      <van-row>
-        <van-col :span="6">学习能力测试</van-col>
-        <van-col :span="6">思维能力测试</van-col>
-        <van-col :span="6">da能力测试</van-col>
-        <van-col :span="6">能力测试</van-col>
-      </van-row>
-    </div>
-    <div style="margin: 16px;">
-      <van-button round block type="primary" native-type="submit">
-        提交
-      </van-button>
-    </div>
-  </van-form>
+
+<router-view/>
+
 </template>
 
 <script lang="ts" setup>
 import {onMounted, reactive, ref} from "vue";
 import request from "@/request/request";
 import { areaList } from '@vant/area-data';
+import {create} from "axios";
+import router from "@/router";
 interface stuInfo {
   stuId: string,
   stuNumber: string,
@@ -89,6 +24,20 @@ interface stuInfo {
   expressAbility: string,
   thinkingAbility: string,
   executeAbility: string
+}
+
+interface question{
+  id:string,
+  content:string,
+  optionA:string,
+  optionB:string,
+  optionC:string,
+  optionD:string,
+  questionType:string,
+  scoreA:number,
+  scoreB:number,
+  scoreC:number,
+  scoreD:number,
 }
 const form: stuInfo = reactive({
   stuId: "",
@@ -105,6 +54,8 @@ const form: stuInfo = reactive({
   thinkingAbility: "",
   executeAbility: ""
 })
+
+const questionTable:question[] = reactive([])
 
 const now_date = new Date()
 const minDate = ref(new Date(1990, 1, 1))
@@ -130,9 +81,27 @@ const onSubmit = () =>{
   // })
 }
 
+const goToAbout = () =>{
+  router.push({
+    path:'/test'
+  })
+}
+
+const test = ()=>{
+  request.get("/question-source-entity/getAllQuestion").then(res => {
+    questionTable.push(...res.data.data)
+    // console.log(questionTable.info)
+    // console.log(questionTable)
+    console.log(questionTable[0])
+  })
+}
+
 onMounted(() => {
   request.get("/question-source-entity/getAllQuestion").then(res => {
-    console.log(res)
+    questionTable.push(...res.data.data)
+    // console.log(questionTable.info)
+    // console.log(questionTable)
+    console.log(questionTable[0])
   })
 })
 </script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,62 @@
+<template>
+  {{questionTable.info}}
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive} from "vue";
+import request from "@/request/request";
+
+
+export default defineComponent({
+  name: "TestView",
+  created() {
+    request.get("/question-source-entity/getAllQuestion").then(res => {
+      this.questionTable.info=res.data.data
+      console.log(res.data.data)
+      // console.log(questionTable.info)
+      // console.log(questionTable)
+      console.log(this.questionTable.info)
+    })
+  },
+  setup(){
+    interface question{
+      id:string,
+      content:string,
+      optionA:string,
+      optionB:string,
+      optionC:string,
+      optionD:string,
+      questionType:string,
+      scoreA:number,
+      scoreB:number,
+      scoreC:number,
+      scoreD:number,
+    }
+    // const questionTable:question[] = reactive([])
+    const questionTable = reactive({
+      info:[{
+        id:'',
+        content:'',
+        optionA:'',
+        optionB:'',
+        optionC:'',
+        optionD:'',
+        questionType:'',
+        scoreA:0,
+        scoreB:0,
+        scoreC:0,
+        scoreD:0,
+      }]
+    })
+
+    return{
+      questionTable,
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
