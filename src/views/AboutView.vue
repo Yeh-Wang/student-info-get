f1d2dd1f34e3b0f2a02c975169e88d0438a70ecd
Yeh-Wang <wyx1627082750@163.com>
2023-03-06 11:52:11 +0800

    YehWang

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,29 +3,6 @@
 </template>
 
 <script lang="ts" setup>
-import {onMounted, reactive} from "vue";
-import request from "@/request/request";
-interface question{
-  id:string,
-  content:string,
-  optionA:string,
-  optionB:string,
-  optionC:string,
-  optionD:string,
-  questionType:string,
-  scoreA:number,
-  scoreB:number,
-  scoreC:number,
-  scoreD:number,
-  answer:string,
-}
-const questionInfo:question[] = reactive([])
 
-onMounted(()=>{
-  request.get("/question-source-entity/getAllQuestion").then(res=>{
-    questionInfo.push(...res.data.data)
-    console.log(questionInfo)
-  })
-})
 </script>
 
